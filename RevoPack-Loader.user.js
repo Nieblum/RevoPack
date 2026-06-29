eb4658276ebd18458053a82885a26ebe760ff033
Nieblum <jakub7304@gmail.com>
2026-06-30 01:44:08 +0200

    Bump version to 1.1 and update RevoPack URL

--- RevoPack-Loader.user.js
+++ RevoPack-Loader.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         RevoPack Loader
 // @namespace    Nieblum
-// @version      1.0
+// @version      1.1
 // @author       Nieblum
 // @description  Loader - pobiera RevoPack z GitHuba.
 // @match        https://*.margonem.pl/
 // @grant        none
-// @require      https://raw.githubusercontent.com/Nieblum/RevoPack/main/RevoPack.user.js?v=1
+// @require      https://raw.githubusercontent.com/Nieblum/RevoPack/main/RevoPack.user.js?v=2
 // ==/UserScript==
