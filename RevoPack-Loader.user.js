632eb3211f22f3ec17c03a51c35a56036a6eae79
Nieblum <jakub7304@gmail.com>
2026-06-30 15:37:15 +0200

    Update version and requirement URL in user script

--- RevoPack-Loader.user.js
+++ RevoPack-Loader.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         RevoPack Loader
 // @namespace    Nieblum
-// @version      1.1
+// @version      1.2
 // @author       Nieblum
 // @description  Loader - pobiera RevoPack z GitHuba.
 // @match        https://*.margonem.pl/
 // @grant        none
-// @require      https://raw.githubusercontent.com/Nieblum/RevoPack/main/RevoPack.user.js?v=2
+// @require      https://raw.githubusercontent.com/Nieblum/RevoPack/main/RevoPack.user.js?v=3
 // ==/UserScript==
